9f1f9e00f2230a683827b327ece6a3122c8c1f54
sasha <sasha@localhost.localdomain>
2017-11-21 00:38:07 +0300

    Output info. auto reload

--- app/src/Cities.js
+++ app/src/Cities.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import City from './City';
+import  './Cities/css/Cities.css'
 class Cities extends Component {
   constructor(props){
      super(props);
@@ -7,20 +8,39 @@ class Cities extends Component {
        cities : props.cities,
        errors: [],
        city: props.city||"",
-       change: props.change || (()=>{}),     
+       change: props.change || (()=>{}),
+       weather: null,
+       weather_date: null,
+       timeout: null,     
      };	  
   }
-/*  componentDidMount(){
-     loadCities()
-     .then((cities)=>{
-        this.setState({cities});
-     }).catch((err)=>{
-        this.setState({errors:[...this.state.errors,err]});
-     });  
-  }*/
   handleChange(e){
     const city = e.target.value;	  
-    this.setState({city}, () => {this.state.change(city)});	  
+    console.log(['change',city]);	  
+    this.setState({city}, () => {
+	    this.state.change(city);
+            this.loadCityWeather(city); 
+    });	  
+  }
+  componentDidMount(){
+    console.log(['mount',this.state.city]);	  
+    this.loadCityWeather(this.state.city);
+  }	
+  loadCityWeather(city){
+      clearTimeout(this.state.timeout);
+      this.setState({timeout:setTimeout(()=>{this.loadCityWeather(city)},1000*60)});	  
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=bd5e378503939ddaee76f12ad7a97608`).then(
+	  (resp) => {
+	     if(resp.ok){
+	       return resp.json();
+	     }
+	  }    
+      ).then((json)=>{
+         this.setState ({
+            weather_date : new Date(),
+            weather : json,		 
+	 });
+      });
   }	
   render() {
     let cities = this.state.cities.map((x,id)=>{
@@ -32,7 +52,14 @@ class Cities extends Component {
         ...cities	    
       ];	  
     }	  
-    return <select value={this.state.city} onChange={this.handleChange.bind(this)}>{cities}</select>
+    return <div className="cities">
+		  <select value={this.state.city} onChange={this.handleChange.bind(this)}>{cities}</select>
+                  <City 
+	             weather={this.state.weather} 
+	             updated={this.state.weather_date}
+	             city={this.state.city} 
+		  />
+           </div>		  
   }
 
 }
---
+++ app/src/City.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './City/css/open-weather-icons.css';
+import './City/css/City.css';
+class City extends Component {
+  render(){
+    let icon, temp;
+    if(this.props.weather){
+        icon = <i className={`owi owi-${this.props.weather.weather[0].icon} city__weather`} />;
+	temp = `${this.props.weather.main.temp}°`;    
+    }
+    	  
+    return <div className="city">
+            {icon}
+	    <div className="city__temp">{temp}</div>
+	  </div> ;
+  }	
+}
+export default City;
