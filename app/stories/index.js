cc6ff7a28d1b2b8165fcedcbefe6910f40bd309f
sasha <sasha@localhost.localdomain>
2017-11-21 22:29:31 +0300

    storybook

---
+++ app/stories/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import City from '../src/City.js'; 
+import Cities from '../src/Cities.js';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+
+/*storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+*/
+storiesOf('City', module)
+    .add('weather', ()=> (<City weather={{weather:[{icon:"09d"}], main:{temp: 40}}}/> ));
+
+storiesOf('Cities', module)
+    .add('Not selected', ()=>(<Cities cities={["London","Moscow"]}/>))
+    .add('Selected', () => (<Cities cities={["London","Moscow"]} city="London"/>))	
+
